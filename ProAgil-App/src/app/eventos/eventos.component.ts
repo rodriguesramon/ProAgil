1c8c0c43d1ec4c2a05235fc375b2f1b2304a594f
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2020-06-01 00:25:15 -0400

    Upload de Imagens Evento

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -27,6 +27,9 @@ export class EventosComponent implements OnInit {
   _filtroLista = '';
   registerForm: FormGroup;
   bodyDeletarEvento = '';
+  file: File;
+  fileNameToUpdate: string;
+  dataAtual: string;
 
   constructor(
     private eventoService: EventoService,
@@ -54,8 +57,10 @@ export class EventosComponent implements OnInit {
   editarEvento(evento: Evento, template: any){
     this.modoSalvar = 'put';
     this.openModal(template);
-    this.evento = evento;
-    this.registerForm.patchValue(evento);
+    this.evento = Object.assign({}, evento);
+    this.fileNameToUpdate = evento.imagemURL.toString();
+    this.evento.imagemURL = '';
+    this.registerForm.patchValue(this.evento);
   }
 
   novoEvento(template: any){
@@ -99,10 +104,41 @@ export class EventosComponent implements OnInit {
     );
   }
 
+  onFileChange(event){
+    const reader = new FileReader();
+    if(event.target.files && event.target.files.length){
+      this.file = event.target.files;
+      console.log(this.file);
+    }
+  }
+
+  uploadImage(){
+    if(this.modoSalvar === "post"){
+      const nomeArquivo = this.evento.imagemURL.split('\\', 3);
+      this.evento.imagemURL = nomeArquivo[2];
+      this.eventoService.postUpload(this.file, nomeArquivo[2]).subscribe(
+        () => {
+          this.dataAtual = new Date().getMilliseconds().toString();
+          this.getEventos();
+        }
+      );
+    }else{
+      this.evento.imagemURL = this.fileNameToUpdate;
+      this.eventoService.postUpload(this.file, this.fileNameToUpdate).subscribe(
+        () => {
+          this.dataAtual = new Date().getMilliseconds().toString();
+          this.getEventos();
+        }
+      );
+    }
+  }
+
   salvarAlteracao(template: any){
     if(this.registerForm.valid){
       if(this.modoSalvar === "post"){
         this.evento = Object.assign({}, this.registerForm.value);
+        this.uploadImage();
+
         this.eventoService.postEvento(this.evento).subscribe(
           (novoEvento: Evento) =>{
             console.log(novoEvento);
@@ -115,6 +151,7 @@ export class EventosComponent implements OnInit {
         );
       }else{
         this.evento = Object.assign({id: this.evento.id}, this.registerForm.value);
+        this.uploadImage();
         this.eventoService.putEvento(this.evento).subscribe(
           (novoEvento: Evento) =>{
             console.log(novoEvento);
--- ProAgil.WebAPI/Controllers/EventoController.cs
+++ ProAgil.WebAPI/Controllers/EventoController.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
@@ -34,6 +36,31 @@ namespace ProAgil.WebAPI.Controllers
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, $"Bando de Dados Falhou {exception.Message}");
             }
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var folderName = Path.Combine("Resources","Images");
+                var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+                if(file.Length > 0){
+                    var filename = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName;
+                    var fullPath = Path.Combine(pathToSave, filename.Replace("\"", " ").Trim());
+                    using(var stream = new FileStream(fullPath, FileMode.Create)){
+                        file.CopyTo(stream);
+                    }
+                }
+                return Ok();
+            }
+            catch (System.Exception exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Bando de Dados Falhou {exception.Message}");
+            }
+            return BadRequest("Erro ao realizar o upload");
         }
 
         [HttpGet("{EventoId}")]
